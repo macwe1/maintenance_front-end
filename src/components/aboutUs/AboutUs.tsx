a6fe64982c5374b5fd25f8f7e741a36016ab11e9
XOA <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>
2022-02-24 22:10:17 +0500

    update

--- src/components/aboutUs/AboutUs.tsx
+++ src/components/aboutUs/AboutUs.tsx
@@ -26,8 +26,87 @@ export const AboutUs: React.FunctionComponent = () => {
               надежных руках!
             </p>
           </div>
-          <div className={styles.wrapper_container_info_picture}>
-            <div className={styles.wrapper_container_info_picture_bg1}></div>
+          <div className={styles.wrapper_container_info_progress}>
+            <div className={styles.wrapper_container_info_progress_bar}>
+              <span className={styles.wrapper_container_info_progress_bar_text}>
+                Клиенты
+              </span>
+              <div className={styles.wrapper_container_info_progress_bar_line}>
+                <div
+                  className={
+                    styles.wrapper_container_info_progress_bar_line_red
+                  }
+                >
+                  <div
+                    className={
+                      styles.wrapper_container_info_progress_bar_line_red_value
+                    }
+                  >
+                    7000
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.wrapper_container_info_progress_bar}>
+              <span className={styles.wrapper_container_info_progress_bar_text}>
+                Завершенных работ
+              </span>
+              <div className={styles.wrapper_container_info_progress_bar_line}>
+                <div
+                  className={
+                    styles.wrapper_container_info_progress_bar_line_red_two
+                  }
+                >
+                  <div
+                    className={
+                      styles.wrapper_container_info_progress_bar_line_red_two_value
+                    }
+                  >
+                    6400
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.wrapper_container_info_progress_bar}>
+              <span className={styles.wrapper_container_info_progress_bar_text}>
+                Деталей на складе
+              </span>
+              <div className={styles.wrapper_container_info_progress_bar_line}>
+                <div
+                  className={
+                    styles.wrapper_container_info_progress_bar_line_red_three
+                  }
+                >
+                  <div
+                    className={
+                      styles.wrapper_container_info_progress_bar_line_red_three_value
+                    }
+                  >
+                    10100
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.wrapper_container_info_progress_bar}>
+              <span className={styles.wrapper_container_info_progress_bar_text}>
+                Заявок
+              </span>
+              <div className={styles.wrapper_container_info_progress_bar_line}>
+                <div
+                  className={
+                    styles.wrapper_container_info_progress_bar_line_red_fouth
+                  }
+                >
+                  <div
+                    className={
+                      styles.wrapper_container_info_progress_bar_line_red_fouth_value
+                    }
+                  >
+                    3500
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
